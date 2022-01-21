090d0be1f997d667cfdd66367685051772bcc95a
MonsieurTweek <gaetan.regent@gmail.com>
2022-01-22 00:08:15 +0100

    NavAgent on Player + experiment on movement based on node controller

---
+++ Assets/MovementController.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum DirectionEnum { None, Top, Right, Bottom, Left};
+public class MovementController : MonoBehaviour
+{
+
+    public float Speed = 4f;
+    public NodeController StartingNode;
+
+    private DirectionEnum _currentDirection;
+    private DirectionEnum _nextDirection;
+    private NodeController _destinationNode;
+    private NavMeshAgent _agent;
+
+    public DirectionEnum CurrentDirection { get => _currentDirection; set => _currentDirection = value; }
+    public DirectionEnum NextDirection { get => _nextDirection; set => _nextDirection = value; }
+    public NavMeshAgent Agent { get => _agent; set => _agent = value; }
+    public NodeController DestinationNode { get => _destinationNode; set => _destinationNode = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentDirection = DirectionEnum.Left;
+        NextDirection = DirectionEnum.None;
+
+        Agent = GetComponent<NavMeshAgent>();
+
+        SetNewDestination(StartingNode.NodeLeft);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Agent.pathStatus == NavMeshPathStatus.PathComplete || Agent.remainingDistance < 0.1f)
+        {
+            NodeController destination;
+
+            // Get new destination
+            if(NextDirection != DirectionEnum.None)
+            {
+                destination = FindNextNode(NextDirection);
+                if(destination == null)
+                {
+                    destination = FindNextNode(CurrentDirection);
+                }
+            } else
+            {
+                destination = FindNextNode(CurrentDirection);
+            }
+
+            if(destination != null)
+            {
+                SetNewDestination(destination);
+
+            // Fake movement input
+            } else
+            {
+                DirectionEnum[] availableDirections = new DirectionEnum[4] { DirectionEnum.Top, DirectionEnum.Right, DirectionEnum.Bottom, DirectionEnum.Left };
+                NextDirection = availableDirections[Random.Range(0, availableDirections.Length)];
+            }
+        }
+
+
+
+    }
+
+    private void SetNewDestination(NodeController destination)
+    {
+        DestinationNode = destination;
+        Agent.SetDestination(destination.gameObject.transform.position);
+    }
+
+    private NodeController FindNextNode(DirectionEnum direction)
+    {
+        NodeController destination = null;
+
+        switch (direction)
+        {
+            case DirectionEnum.Top:
+                if (DestinationNode.CanMoveTop == true)
+                {
+                    destination = DestinationNode.NodeTop;
+                }
+                break;
+
+            case DirectionEnum.Right:
+                if (DestinationNode.CanMoveRight == true)
+                {
+                    destination = DestinationNode.NodeRight;
+                }
+                break;
+
+            case DirectionEnum.Bottom:
+                if (DestinationNode.CanMoveBottom == true)
+                {
+                    destination = DestinationNode.NodeBottom;
+                }
+                break;
+
+            case DirectionEnum.Left:
+                if (DestinationNode.CanMoveLeft == true)
+                {
+                    destination = DestinationNode.NodeLeft;
+                }
+                break;
+        }
+
+        return destination;
+    }
+}
---
+++ Assets/NodeController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeController : MonoBehaviour
+{
+
+    
+    private bool _canMoveTop = false;
+    private bool _canMoveRight = false;
+    private bool _canMoveBottom = false;
+    private bool _canMoveLeft = false;
+
+    private NodeController _nodeTop;
+    private NodeController _nodeRight;
+    private NodeController _nodeBottom;
+    private NodeController _nodeLeft;
+
+    public bool CanMoveTop { get => _canMoveTop; set => _canMoveTop = value; }
+    public bool CanMoveRight { get => _canMoveRight; set => _canMoveRight = value; }
+    public bool CanMoveBottom { get => _canMoveBottom; set => _canMoveBottom = value; }
+    public bool CanMoveLeft { get => _canMoveLeft; set => _canMoveLeft = value; }
+    public NodeController NodeTop { get => _nodeTop; set => _nodeTop = value; }
+    public NodeController NodeRight { get => _nodeRight; set => _nodeRight = value; }
+    public NodeController NodeBottom { get => _nodeBottom; set => _nodeBottom = value; }
+    public NodeController NodeLeft { get => _nodeLeft; set => _nodeLeft = value; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RaycastHit hit;
+        // Top Node
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1f))
+        {
+            if (hit.transform.gameObject.tag == "Node")
+            {
+                NodeTop = hit.transform.gameObject.GetComponent<NodeController>();
+                CanMoveTop = true;
+            }
+        }
+        // Right Node
+        if (Physics.Raycast(transform.position, -1f * transform.TransformDirection(Vector3.left), out hit, 1f))
+        {
+            if (hit.transform.gameObject.tag == "Node")
+            {
+                NodeRight = hit.transform.gameObject.GetComponent<NodeController>();
+                CanMoveRight = true;
+            }
+        }
+        // Bottom Node
+        if (Physics.Raycast(transform.position, -1f * transform.TransformDirection(Vector3.forward), out hit, 1f))
+        {
+            if (hit.transform.gameObject.tag == "Node")
+            {
+                NodeBottom = hit.transform.gameObject.GetComponent<NodeController>();
+                CanMoveBottom = true;
+            }
+        }
+        // Left Node
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, 1f))
+        {
+            if(hit.transform.gameObject.tag == "Node")
+            {
+                NodeLeft = hit.transform.gameObject.GetComponent<NodeController>();
+                CanMoveLeft = true;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
